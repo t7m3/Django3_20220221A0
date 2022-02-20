c115802a4e2695ed7dc2981cb2e93171ee3bce79
DESKTOP-IKVRJMR\standard <toda7.mk3@gmail.com>
2022-02-21 05:29:51 +0900

    0001 2022-02-21

---
+++ hello/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import HelloForm
+
+# Create your views here.
+
+def index(request):
+    params = {
+        'title':'Hello',
+        'message':'your data:',
+        'form':HelloForm()
+    }
+    
+    if (request.method == 'POST'):
+        params['message'] = '名前:' + request.POST['name'] + \
+            '<br>メール:' + request.POST['mail'] + \
+            '<br>年齢:' + request.POST['age']
+        params['form'] = HelloForm(request.POST)
+
+    return render(request, 'hello/index.html', params)
+
+
