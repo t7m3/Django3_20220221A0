c3cb65756ac6576ad73f48b0812b05a5db69353a
DESKTOP-IKVRJMR\standard <toda7.mk3@gmail.com>
2022-03-09 16:43:18 +0900

    0017 2022-03-09  p173まで済み。

--- hello/views.py
+++ hello/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Friend
+from .forms import HelloForm
 
 
 # Create your views here.
@@ -10,8 +11,17 @@ def index(request):
     params = {
         'title':'Hello',
         'message':'all friends.',
+        'form':HelloForm(),
         'data': data,
     }
+
+    if (request.method == 'POST'):
+        num=request.POST['id']
+        item = Friend.objects.get(id=num)
+        params['data'] = [item]
+        params['form'] = HelloForm(request.POST)
+    else:
+        params['data'] = Friend.objects.all()
 
     return render(request, 'hello/index.html', params)
 
