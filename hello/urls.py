d5fe89b7d1836b287d65d747cee0da4b66f95e11
DESKTOP-IKVRJMR\standard <toda7.mk3@gmail.com>
2022-03-09 15:26:23 +0900

    0016 2022-03-09  p170まで済み。間違いが見つかった。bootstrapを、ここまで、ずーっと間違えていた。今回修正した（p167に記録した）。

--- hello/urls.py
+++ hello/urls.py
@@ -1,8 +1,7 @@
-
-from django.conf.urls import url
-from .views import HelloView
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    url(r'', HelloView.as_view(), name='index'),
+    path('', views.index, name='index'),
     
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,31 +1,19 @@
-from unittest import result
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import TemplateView
-from .forms import HelloForm
+from .models import Friend
+
 
 # Create your views here.
 
-class HelloView(TemplateView):
+def index(request):
+    data = Friend.objects.all()
+    params = {
+        'title':'Hello',
+        'message':'all friends.',
+        'data': data,
+    }
 
-    def __init__(self):
-        self.params = {
-            'title':'Hello',
-            'form':HelloForm(),
-            'result':''  # None
-        }
+    return render(request, 'hello/index.html', params)
 
-    def get(self, request):
-        return render(request, 'hello/index.html', self.params)
-
-    def post(self, request):
-        ch = request.POST.getlist('choice')
-        result = 'selected:\n'  # index.htmlを{{result | linebreaksbr}}と変更しないと、改行しない。
-        for item in ch:
-            result += '  ・'  + item + '\n'
-        # result += '\n'
-        self.params['result'] = result
-        self.params['form'] = HelloForm(request.POST)
-        return render(request, 'hello/index.html', self.params)
 
 
